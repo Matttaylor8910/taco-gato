49dd6e34fb6cc1a8f19876721d1f12a9e3f2e5b8
Daniel Ryan <daniel.c.ryan@icloud.com>
2018-03-11 19:09:35 -0600

    Forgot to add leaderboard-list-item

---
+++ www/js/components/leaderboard-list-item/leaderboard-list-item.component.js
@@ -0,0 +1,11 @@
+(function () {
+  angular
+    .module('taco')
+    .component('leaderboardListItem', {
+      templateUrl: 'js/components/leaderboard-list-item/leaderboard-list-item.tpl.html',
+      bindings: {
+        eater: '<',
+        position: '<',
+      }
+    });
+})();
